ccfabb87326bacceb44d1277bcc999cc77944960
Ubuntu <ubuntu@ip-172-31-26-220.ap-northeast-1.compute.internal>
2016-11-23 14:59:23 +0800

    first commit!

---
+++ testgit.js
@@ -0,0 +1,7 @@
+    require('simple-git')()
+         .init()
+         .add('./*')
+         .commit("first commit!")
+         .addRemote('origin', 'https://github.com/pedroid/forfun.git')
+         .push('origin', 'master');
+
